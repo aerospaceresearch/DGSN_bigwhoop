81832ac6a7083ee7e8bf0a518ad305ca7698822a
Michael Schupikov <michael.schupikov@aerospaceresearch.net>
2015-03-28 21:11:21 +0100

    Add class for proper logging

---
+++ server/log.cpp
@@ -0,0 +1,34 @@
+#include "log.hpp"
+
+bool log::m_bInitialised;
+int  log::m_nLevel;
+
+void log::write(int nLevel, const char *szFormat, ...) noexcept
+{
+    checkInit();
+    if (nLevel >= m_nLevel){
+        va_list args;
+        va_start(args, szFormat);
+        vprintf(szFormat, args);
+        va_end(args);
+    }
+}
+
+void log::setLevel(int nLevel) noexcept
+{
+    m_nLevel = nLevel;
+    m_bInitialised = true;
+}
+
+void log::checkInit() noexcept
+{
+    if(!m_bInitialised){
+        init();
+    }
+}
+
+void log::init() noexcept
+{
+    setLevel(log::all);
+}
+
---
+++ server/log.hpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <cstdarg>
+
+class log
+{
+  public:
+    enum { all=0, verbose, debug, info, warning, error, fatal, none };
+    static void write(int nLevel, const char *szFormat, ...) noexcept;
+    static void setLevel(int nLevel) noexcept;
+
+  protected:
+    static void checkInit() noexcept;
+    static void init() noexcept;
+
+  private:
+    log() = delete;
+    ~log() = delete;
+    log& operator= (const log&) = delete;
+    log& operator= (const log&&) = delete;
+    
+    static bool m_bInitialised;
+    static int  m_nLevel;
+};
+
