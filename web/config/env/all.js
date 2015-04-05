8fffca77a9283a75560f704bdfd846867112e9e4
bgp92 <bpg_92@hotmail.com>
2015-04-05 10:06:54 -0400

    added data, made the map, working more on UI from now on

--- web/config/env/all.js
+++ web/config/env/all.js
@@ -25,6 +25,8 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
+				'public/lib/heatmap.js.bower/src/heatmap.js',
+				'public/lib/heatmap.js.bower/src/gmaps-heatmap.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
 			]
 		},
--- web/public/modules/core/controllers/home.client.controller.js
+++ web/public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,53 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
-		// This provides Authentication context.
-		$scope.authentication = Authentication;
-	}
+angular.module('core').controller('HomeController', ['$scope','$http', 'Authentication',
+    function($scope, $http, Authentication) {
+        // This provides Authentication context.
+        $scope.authentication = Authentication;
+
+        // map center
+        var myLatlng = new google.maps.LatLng(52.4935, 7.629);
+        // map options,
+        var myOptions = {
+            zoom: 3,
+            center: myLatlng
+        };
+        // standard map
+        $scope.map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+	        // heatmap layer
+	        $scope.heatmap = new HeatmapOverlay($scope.map, {
+	            // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+	            "radius": 4,
+	            "maxOpacity": 1,
+	            // scales the radius based on map zoom
+	            "scaleRadius": true,
+	            // if set to false the heatmap uses the global maximum for colorization
+	            // if activated: uses the data maximum within the current map boundaries 
+	            //   (there will always be a red spot with useLocalExtremas true)
+	            "useLocalExtrema": true,
+	            // which field name in your data represents the latitude - default "lat"
+	            latField: 'lat',
+	            // which field name in your data represents the longitude - default "lng"
+	            lngField: 'lon',
+	            // which field name in your data represents the data value - default "value"
+	            valueField: 'max_amplitude'
+	        });
+		    
+        $http.get('modules/core/json/bigwhoop.json').success(function(bigwhoop) {
+		    console.warn(bigwhoop.data.dataset);
+		    var testData = {
+	            max: 100000,
+	            data: bigwhoop.data.dataset.analyze_full_spectrum_basic
+	            //bigwhoop.data.dataset
+	            // [{
+	            //     lat: 24.6408,
+	            //     lng: 46.7728,
+	            //     count: 14
+	            // }]
+	        };
+	        $scope.heatmap.setData(testData);
+		});
+        
+    }
 ]);
