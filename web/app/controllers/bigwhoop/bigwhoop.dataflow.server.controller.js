d65d8573b411df48efadb4ac4c3ed40837a5b63a
bgp92 <bpg_92@hotmail.com>
2015-04-12 09:42:43 -0400

    added response to the request

--- web/app/controllers/bigwhoop/bigwhoop.dataflow.server.controller.js
+++ web/app/controllers/bigwhoop/bigwhoop.dataflow.server.controller.js
@@ -10,5 +10,19 @@ var _ = require('lodash'),
  * Recieve data and try to propagate
  */
 exports.dataflow = function(req, res) {
+	var bigWhoopData = req.body;
+	console.warn(bigWhoopData);
+	if(bigWhoopData){
 
+
+		res.status(200).send({
+			message: 'Thanks for your submission'
+		});
+	}
+	else{
+		res.status(400).send({
+			message: 'Can\'t get your data at the moment'
+		});
+	}
+	
 };
