2fe010bcfeae71af6ae4be656521e27df7321927
Michael Schupikov <michael.schupikov@aerospaceresearch.net>
2015-04-12 15:19:23 +0200

    Improve server

--- server/server.py
+++ server/server.py
@@ -6,8 +6,27 @@ import os
 import sqlite3
 import json
 
+import json
+from functools import wraps
+from flask import redirect, request, current_app
+
+
 app = Flask(__name__)
 app.config['DB_FILE'] = config.DB_FILE
+
+def support_jsonp(f):
+    """Wraps JSONified output for JSONP"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            content = str(callback) + '(' + str(f().data) + ')'
+            return current_app.response_class(content,\
+                mimetype='application/json')
+        else:
+            return f(*args, **kwargs)
+    return decorated_function
+
 
 def allowed_file(filename):
     return filename.endswidth('.db');
@@ -33,6 +52,7 @@ def dict_factory(cursor, row):
     return d
 
 @app.route('/data/<int:freq>', methods=['GET'])
+@support_jsonp
 def reply_data(freq):
     connection = sqlite3.connect(config.DB_FILE)
     connection.row_factory = dict_factory
@@ -40,16 +60,16 @@ def reply_data(freq):
     cursor.execute(
     """
     SELECT DISTINCT
-    client_id_hash, time, location_lat, location_lon,
-    MAX(amp_mean) AS amp_mean, amp_max
+    location_lat AS lat, location_lon AS lng,
+    amp_mean AS mean, amp_max
     FROM data
     WHERE freq = %s
     GROUP BY
         client_id_hash;
     """ % freq)
-    ret = json.dumps(cursor.fetchall())
+    ret = cursor.fetchall()
     connection.close()
-    return ret
+    return jsonify(ret)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5001, debug=True)
